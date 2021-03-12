25c11a39c654c7ab792c38bc4ca159a24d6096e7
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 21:15:11 +0300

    Update AND_32.vhd

--- AND_32.vhd
+++ AND_32.vhd
@@ -1,15 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity and_32 is
+entity and32 is
   port (
     a   : in  std_logic_vector (31 downto 0) := (others => '0') ;
     b   : in  std_logic_vector (31 downto 0) := (others => '0');
     c   : out std_logic_vector (31 downto 0) := (others => '0')
   );
-end and_32;
+end and32;
 
-architecture behavioral of and_32 is
+architecture behavioral of and32 is
 begin
   c <= a and b;
 end behavioral;
