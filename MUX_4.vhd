3ae9fd4bd8048add869018f9669e544dfec28bf0
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 21:13:15 +0300

    Update MUX_4.vhd

--- MUX_4.vhd
+++ MUX_4.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-entity mux_4 is
+entity mux4 is
   port(
     subs1      : in  std_logic_vector(31 downto 0) := (others => '0');
     subs2      : in  std_logic_vector(31 downto 0) := (others => '0');
     subs3      : in  std_logic_vector(31 downto 0) := (others => '0');
     subs4      : in  std_logic_vector(31 downto 0) := (others => '0');
     selAluc    : in  std_logic_vector(1 downto 0)  := '00';
-    r          : out std_logic_vector(31 downto 0) := (others => '0');
-  end mux_4;
-architecture rtl of mux_4 is
+    mux4Out    : out std_logic_vector(31 downto 0) := (others => '0');
+  end mux4;
+architecture rtl of mux4 is
 begin
 p_mux : process(subs1, subs2, subs3, subs4, selAluc)
 begin
