e8bcfb971ea9527be27c9620309b4a1899d5dbaa
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 18:43:57 +0300

    Create OR_32.vhd

---
+++ OR_32.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity or_32 is
+  port (
+    a   : in  std_logic_vector(31 downto 0);
+    b   : in  std_logic_vector(31 downto 0);
+    c   : out std_logic_vector(31 downto 0)
+  );
+end or_32;
+
+architecture behavioral of or_32 is
+begin
+  c <= a or b;
+end behavioral;
