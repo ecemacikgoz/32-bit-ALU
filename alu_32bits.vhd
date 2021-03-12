1cdda81f94db61a4d2c19bd5aad0929ddb6ea4b6
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 19:03:50 +0300

    Update alu_32bits.vhd

--- alu_32bits.vhd
+++ alu_32bits.vhd
@@ -3,15 +3,18 @@ use ieee.std_logic_1164.all;
 
 entity alu_32bits is
   port (
-    a    : in  std_logic_vector(31 downto 0);
-    b    : in  std_logic_vector(31 downto 0);
-    aluc : in std_logic_vector(3 downto 0);
-    r    : out std_logic_vector(31 downto 0);
-    z    : out std_logic
+    a    : in  std_logic_vector(31 downto 0) := (others => '0');
+    b    : in  std_logic_vector(31 downto 0) := (others => '0');
+    aluc : in std_logic_vector(3 downto 0) := (others => '0');
+    r    : out std_logic_vector(31 downto 0) := (others => '0');
+    z    : out std_logic := '0'
   );
 end alu_32bits;
 
 architecture behavioral of alu_32bits is
+signal rTemp  : std_logic_vector (31 downto 0) := (others => '0');
+signal zTemp : std_logic := '0'; 
+
 begin
     
     
