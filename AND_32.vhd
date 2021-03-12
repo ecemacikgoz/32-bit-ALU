e124088713eba509583870c38e4eef9e94e9b2fc
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 18:59:07 +0300

    Update AND_32.vhd

--- AND_32.vhd
+++ AND_32.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity and_32 is
   port (
-    a   : in  std_logic_vector(31 downto 0);
-    b   : in  std_logic_vector(31 downto 0);
-    c   : out std_logic_vector(31 downto 0)
+    a   : in  std_logic_vector (31 downto 0) := (others => '0') ;
+    b   : in  std_logic_vector (31 downto 0) := (others => '0');
+    c   : out std_logic_vector (31 downto 0) := (others => '0')
   );
 end and_32;
 
