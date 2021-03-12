9d85e33a0d1a5b095c91469be3b5f2e49da9b417
ecemacikgoz <61416666+ecemacikgoz@users.noreply.github.com>
2021-03-12 21:16:22 +0300

    Update XOR_32.vhd

--- XOR_32.vhd
+++ XOR_32.vhd
@@ -1,15 +1,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity xor_32 is
+entity xor32 is
   port (
-    a   : in  std_logic_vector(31 downto 0);
-    b   : in  std_logic_vector(31 downto 0);
-    c   : out std_logic_vector(31 downto 0)
+    a   : in  std_logic_vector(31 downto 0) := (others => '0');
+    b   : in  std_logic_vector(31 downto 0) := (others => '0');
+    c   : out std_logic_vector(31 downto 0) := (others => '0')
   );
-end xor_32;
+end xor32;
 
-architecture behavioral of xor_32 is
+architecture behavioral of xor32 is
 begin
   c <= a xor b;
 end behavioral;
